66052cd95dcf047a872ec68c9c01ffd6ac84a76d
Harsh Pareek <harshpareek91@gmail.com>
2024-06-10 11:24:36 +0530

    version upgrade

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,3 @@
-import { ServerActionError } from "../../dist";
-
 export * from "./action";
 export * from "./types";
 export * from "./errors";
-
-throw new ServerActionError({
-  message: "hi",
-  code: "CONFLICT",
-});
